41bfbd7a4702a6bcfc355f53a0398a67730250ae
Emmanuel Tamenut <tamen012@umn.edu>
2025-04-18 15:15:48 -0500

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+import math
+
+# game settings
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+PLAYER_POS = 1.5, 5 # mini_map
+PLAYER_ANGLE = 0
+PLAYER_SPEED = 0.004
+PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
